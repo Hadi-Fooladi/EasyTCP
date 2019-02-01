914a14b5b3e00efdbf7ed04944ee96c6701a3ea1
Hadi Fooladi Talari <hadi.f7@gmail.com>
2019-02-01 11:11:07 -0500

    v7.3.32 (Adding 'struct' to 'IOAttribute' targets)

--- Source/EasyTCP/EasyTCP/Attributes/IOAttribute.cs
+++ Source/EasyTCP/EasyTCP/Attributes/IOAttribute.cs
@@ -5,7 +5,7 @@ namespace EasyTCP.Attributes
 	/// <summary>
 	/// Used for doing customized IO (e.g. huge arrays)
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class)]
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 	public class IOAttribute : Attribute
 	{
 		/// <param name="IOType">Must implement 'ITypeIO' and must have a default constructor</param>
